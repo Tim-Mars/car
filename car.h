4b6210d5b4b710f93b9d8576c21096482bed3223
unknown <tim_marsho@hotmail.com>
2022-01-12 14:00:58 -0700

    2nd commit car.h

--- car.h
+++ car.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 
 #ifndef car_h
 #define car_h
@@ -8,6 +9,8 @@ class Car{
     private:    
         bool operable;
         int milage;
+        std::vector<std::array> {};
+
     public:
         bool isOperable(Car& c);
         int getMilage();
